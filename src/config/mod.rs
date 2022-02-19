00b9ecbdf3f42381d84dd2ca2ffdb7aff5ce58af
willyrgf <willyr.goncalves@gmail.com>
2022-02-19 18:30:55 -0300

    impl to_raw to wallet

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,14 +1,26 @@
 use std::collections::HashMap;
 
+use rustc_hex::FromHex;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Wallet {
-    import_private_key: String,
+    private_key: String,
 }
+
+impl Wallet {
+    pub fn to_raw(&self) -> Vec<u8> {
+        self.private_key.from_hex().unwrap()
+    }
+}
+
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
-pub struct General {
-    wallets: HashMap<String, Wallet>,
+pub struct Wallets(HashMap<String, Wallet>);
+
+impl Wallets {
+    pub fn get(&self, key: &str) -> &Wallet {
+        self.0.get(key).unwrap()
+    }
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
@@ -37,10 +49,10 @@ pub struct Exchange {
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Config {
-    general: General,
-    assets: HashMap<String, Asset>,
-    networks: HashMap<String, Network>,
-    exchanges: HashMap<String, Exchange>,
+    pub wallets: Wallets,
+    pub assets: HashMap<String, Asset>,
+    pub networks: HashMap<String, Network>,
+    pub exchanges: HashMap<String, Exchange>,
 }
 
 impl Config {
