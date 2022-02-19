9a77ff8eb9dd4366edb530d5e55e943fc6c86bc3
willyrgf <willyr.goncalves@gmail.com>
2022-02-19 14:41:01 -0300

    show config from a yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+use mfm::config::Config;
+
+// multiverse finance machine cli
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    // filename with the mfm configurations
+    #[clap(short, long)]
+    config_filename: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let config = Config::from_file(&args.config_filename);
+
+    println!("config: {:?}", config);
 }
