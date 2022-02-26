47a244a76b3db4dfccc618c08089ac0c3a8fb0b4
willyrgf <willyr.goncalves@gmail.com>
2022-02-26 13:35:05 -0300

    implement map types for all types and some getters

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -3,6 +3,32 @@ use std::collections::HashMap;
 use rustc_hex::FromHex;
 use serde::{Deserialize, Serialize};
 
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct Asset {
+    network_id: String,
+    base: String,
+    quote: String,
+    pair_address: String,
+    exchange_id: String,
+}
+
+impl Asset {
+    pub fn pair_address(&self) -> &str {
+        self.pair_address.as_str()
+    }
+    pub fn exchange_id(&self) -> &str {
+        self.exchange_id.as_str()
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct Assets(HashMap<String, Asset>);
+impl Assets {
+    pub fn get(&self, key: &str) -> &Asset {
+        self.0.get(key).unwrap()
+    }
+}
+
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Wallet {
     private_key: String,
@@ -16,20 +42,10 @@ impl Wallet {
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Wallets(HashMap<String, Wallet>);
-
 impl Wallets {
     pub fn get(&self, key: &str) -> &Wallet {
         self.0.get(key).unwrap()
     }
-}
-
-#[derive(Debug, PartialEq, Deserialize, Serialize)]
-pub struct Asset {
-    network_id: String,
-    base: String,
-    quote: String,
-    pair_address: String,
-    exchange_id: String,
 }
 
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
@@ -41,18 +57,46 @@ pub struct Network {
     blockexplorer_url: String,
 }
 
+impl Network {
+    pub fn rpc_url(&self) -> &str {
+        self.rpc_url.as_str()
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct Networks(HashMap<String, Network>);
+impl Networks {
+    pub fn get(&self, key: &str) -> &Network {
+        self.0.get(key).unwrap()
+    }
+}
+
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Exchange {
     name: String,
     router_address: String,
 }
 
+impl Exchange {
+    pub fn name(&self) -> &str {
+        self.name.as_str()
+    }
+}
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+pub struct Exchanges(HashMap<String, Exchange>);
+impl Exchanges {
+    pub fn get(&self, key: &str) -> &Exchange {
+        self.0.get(key).unwrap()
+    }
+}
+
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct Config {
     pub wallets: Wallets,
-    pub assets: HashMap<String, Asset>,
-    pub networks: HashMap<String, Network>,
-    pub exchanges: HashMap<String, Exchange>,
+    pub assets: Assets,
+    pub networks: Networks,
+    pub exchanges: Exchanges,
 }
 
 impl Config {
