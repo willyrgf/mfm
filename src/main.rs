c5f9c051f4ba1bd6ad27e90df9bc756772e6b822
willyrgf <willyr.goncalves@gmail.com>
2022-02-26 19:11:37 -0300

    move config_file to res

--- src/main.rs
+++ src/main.rs
@@ -20,21 +20,16 @@ struct Args {
 async fn main() -> web3::contract::Result<()> {
     let args = Args::parse();
     let config = Config::from_file(&args.config_filename);
-    // println!("config: {:?}", config);
 
     let wallet = config.wallets.get("test-wallet");
-
-    // let secret = match SecretKey::from_raw(&wallet.to_raw()) {
-    //     Ok(s) => s,
-    //     Err(e) => panic!("invalid secret, err: {}", e),
-    // };
 
     let secret = SecretKey::from_str(&wallet.private_key()).unwrap();
 
     let bsc_network = config.networks.get("bsc");
     let anonq_asset = config.assets.get("anonq");
 
-    let abi_path = |name: &str| format!("./config_files/exchanges/{}/abi.json", name);
+    // TODO: move it to exchange
+    let abi_path = |name: &str| format!("./res/exchanges/{}/abi.json", name);
     // let abi_path = |name: &str| -> &str { format!("../exchanges/{}/abi.json", name).as_str() };
 
     let pk_exchange = config.exchanges.get(anonq_asset.exchange_id());
@@ -54,45 +49,19 @@ async fn main() -> web3::contract::Result<()> {
 
     let path = abi_path(pk_exchange.name());
     let json = abi_json(path.as_str());
-    let public_addr = format!("0x{}", public.to_string());
 
-    println!(
-        "public_addr(): {}, public.to_string(): {}, public: {:?}",
-        public_addr,
-        public.to_string(),
-        public
-    );
     let contract = Contract::from_json(client.eth(), address, json.as_bytes()).unwrap();
-    // let my_account = Address::from_str("0x327bd0E528c4c1a883F0fC25ABEbf2A07a9433cE").unwrap();
-    // let x = public_key_address(&public);
-    // let acc = web3::signing::feature_gated::public_key_address(public);
-    let acc = signing::public_key_address(&public);
+    let account_address = signing::public_key_address(&public);
 
-    // let account = hex!(public.address());
-    // let my_account = hex!("d028d24f16a8893bd078259d413372ac01580769").into();
-
-    let result = contract.query("balanceOf", (acc,), None, Options::default(), None);
+    let result = contract.query(
+        "balanceOf",
+        (account_address,),
+        None,
+        Options::default(),
+        None,
+    );
     let balance_of: U256 = result.await?;
     println!("balance_of: {:?}", balance_of);
-
-    // let name = exchange.name();
-
-    // println!("name: {}", abi_path(pk_exchange.name().to_string()));
-    // println!(
-    //     "include_bytes: {}",
-    //     include_bytes!(abi_path(pk_exchange.name()))
-    // );
-
-    // let address = Address::from_str(public.address().);
-    // let eth = web3::et
-
-    // println!(
-    //     "secret: {:?}; public: {:?}; address: {:?}",
-    //     secret,
-    //     public,
-    //     public.address()
-    // );
-    // println!("address: {:?}", address);
 
     Ok(())
 }
