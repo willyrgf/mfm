4cd6926f55c5b3ee248f4600bfc830a036cabb29
willyrgf <willyr.goncalves@gmail.com>
2022-03-05 17:48:42 -0300

    impl key/address methods for wallet

--- src/config/wallet.rs
+++ src/config/wallet.rs
@@ -1,7 +1,14 @@
-use std::collections::HashMap;
+use crate::signing;
 
-use rustc_hex::{FromHex};
+use rustc_hex::FromHex;
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
 use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, str::FromStr};
+use web3::{
+    transports::Http,
+    types::{Address, U256},
+};
+
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Wallet {
     private_key: String,
@@ -13,6 +20,27 @@ impl Wallet {
     }
     pub fn private_key(&self) -> String {
         self.private_key.clone()
+    }
+    pub fn secret(&self) -> SecretKey {
+        SecretKey::from_str(&self.private_key()).unwrap()
+    }
+    pub fn public(&self) -> PublicKey {
+        let secp = Secp256k1::new();
+        let s = self.secret();
+        let public = PublicKey::from_secret_key(&secp, &s);
+        public
+    }
+    pub fn address(&self) -> Address {
+        let a = signing::public_key_address(&self.public());
+        a
+    }
+    pub async fn nonce(&self, client: web3::Web3<Http>) -> U256 {
+        let n = client
+            .eth()
+            .transaction_count(self.address(), None)
+            .await
+            .unwrap();
+        n
     }
 }
 
