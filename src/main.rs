e7c92b819954b681762d63c6d62f22294d6a0484
willyrgf <willyr.goncalves@gmail.com>
2022-03-06 13:04:43 -0300

    move the cmd to a mod

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,12 @@
-use clap::Command;
-use mfm::config::Config;
+use mfm::{cmd, config::Config};
 use web3::types::U256;
 
 //TODO: handle with all unwraps
 #[tokio::main]
 async fn main() -> web3::contract::Result<()> {
+    let cmd = cmd::new();
+
     // let args = Args::parse();
-
-    let cmd = Command::new("mfm")
-        .bin_name("mfm")
-        .arg(
-            clap::arg!(-c - -config_filename <PATH> "Config file path")
-                .required(false)
-                .default_value("config.yaml"),
-        )
-        .subcommand_required(true)
-        .subcommand(
-            Command::new("wrap")
-                .about("Wrap a coin to a token")
-                .arg(
-                    clap::arg!(--"network" <bsc> "Network to wrap coin to token")
-                        .required(true),
-                )
-                .arg(
-                    clap::arg!(--"wallet" <WALLET_NAME> "Wallet id from config file")
-                        .required(true),
-                )
-                .arg(
-                    clap::arg!(--"amount" <AMMOUNT> "Amount to wrap coin into token, default: (balance-min_balance_coin)")
-                        .required(false)
-                        ,
-                ),
-        )
-        .subcommand(
-            Command::new("swaptt")
-                .about("Swap Tokens for Tokens")
-                .arg(
-                    clap::arg!(-e --"exchange" <pancake_swap_v2> "Exchange to use router")
-                        .required(true),
-                )
-                .arg(
-                    clap::arg!(-w --"wallet" <WALLET_NAME> "Wallet id from config file")
-                        .required(true),
-                )
-                .arg(
-                    clap::arg!(-a --"amount" <AMMOUNT> "Amount of TokenA to swap to TokenB")
-                        .required(false)
-                )
-                .arg(
-                    clap::arg!(-i --"token_input" <TOKEN_INPUT> "Asset of input token")
-                        .required(false)
-                )
-                .arg(
-                    clap::arg!(-o --"token_output" <TOKEN_OUTPUT> "Asset of output token")
-                        .required(false)
-                ),
-
-        );
-
     let cmd_matches = cmd.get_matches();
     println!("matches: {:?}", cmd_matches);
     let config = match cmd_matches.value_of("config_filename") {
