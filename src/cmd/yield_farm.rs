b4016aad77010a43a1783e76eda41e09be2fc0e5
Antonio Roberto Silva <forevertonny@gmail.com>
2022-03-19 16:20:25 -0300

    add min reward required before harvest

--- src/cmd/yield_farm.rs
+++ src/cmd/yield_farm.rs
@@ -5,15 +5,21 @@ pub const YIELD_FARM_COMMAND: &'static str = "yield-farm";
 
 pub async fn call_sub_commands(args: &ArgMatches, config: &config::Config) {
     let yield_farm = cmd::get_yield_farm(args, config);
-
     let network = yield_farm.get_network(config);
     let client = network.get_web3_client_http();
-
-    //let gas_price = client.eth().gas_price().await.unwrap();
-    log::debug!("yield_farm: {:?}", yield_farm);
+    let yield_farm_asset = yield_farm.get_asset(config);
+    let yield_farm_asset_decimals = yield_farm_asset.decimals(client.clone()).await;
 
     let pending_rewards = yield_farm.get_pending_rewards(config, client.clone()).await;
-    log::debug!("yield_farm pending rewards: {:?}", pending_rewards);
+    let min_rewards_required = yield_farm.get_min_rewards_required_u256(yield_farm_asset_decimals);
+    log::info!("yield_farm pending rewards: {:?}", pending_rewards);
+    log::info!(
+        "yield_farm min rewards required: {:?}",
+        min_rewards_required
+    );
 
-    yield_farm.harvest(config, client.clone()).await;
+    if pending_rewards >= min_rewards_required {
+        log::info!("harvesting yield farm: {:?}", yield_farm);
+        yield_farm.harvest(config, client.clone()).await;
+    }
 }
--- src/config/yield_farm.rs
+++ src/config/yield_farm.rs
@@ -7,7 +7,7 @@ use web3::{
     types::{Address, U256},
 };
 
-use super::{network::Network, wallet::Wallet, Config};
+use super::{asset::Asset, network::Network, wallet::Wallet, Config};
 
 pub mod posi_farm_bnb_posi;
 pub mod posi_farm_busd_posi;
@@ -20,12 +20,31 @@ pub struct YieldFarm {
     address: String,
     operation: String,
     network_id: String,
+    asset_id: String,
+    min_rewards_required: f64,
+    //quoted_asset_id: String,
 }
 
 impl YieldFarm {
     pub fn address(&self) -> String {
         self.address.clone()
     }
+
+    pub fn get_asset<'a>(&self, config: &'a Config) -> &'a Asset {
+        let asset = config.assets.get(self.asset_id.as_str());
+        asset
+    }
+
+    pub fn get_min_rewards_required_u256(&self, asset_decimals: u8) -> U256 {
+        let q = self.min_rewards_required;
+        let qe = (q * 10_f64.powf(asset_decimals.into())) as u128;
+        U256::from(qe)
+    }
+
+    // pub fn get_quoted_asset<'a>(&self, config: &'a Config) -> &'a Asset {
+    //     let quoted_asset = config.assets.get(self.quoted_asset_id.as_str());
+    //     quoted_asset
+    // }
 
     pub fn get_wallet<'a>(&self, config: &'a Config) -> &'a Wallet {
         let wallet = config.wallets.get(self.wallet_id.as_str());
