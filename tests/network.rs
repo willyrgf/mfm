9d86ed525fc575e9ff420a4f1555e2e454a4667b
willyrgf <willyr.goncalves@gmail.com>
2022-10-18 20:48:26 -0300

    test: improve test cases for get_exchange_by_liquidity

--- tests/network.rs
+++ tests/network.rs
@@ -6,38 +6,61 @@ use web3::types::U256;
 
 #[tokio::test]
 async fn test_get_exchange_by_liquidity() {
-    let app = App::new();
+    struct TestCase {
+        network_name: &'static str,
+        input_asset_name: &'static str,
+        output_asset_name: &'static str,
+        exchange_expected: &'static str,
+    }
 
+    let app = App::new();
     let config = app.config();
 
-    let busd_config = config.assets.get("busd").unwrap();
-    let busd_bsc_assets: Vec<Asset> = busd_config
-        .new_assets_list()
-        .unwrap()
-        .into_iter()
-        .filter(|a| a.network_id() == "bsc")
-        .collect();
+    let test_cases = vec![
+        TestCase {
+            network_name: "polygon",
+            input_asset_name: "usdc",
+            output_asset_name: "bal",
+            exchange_expected: "sushi_swap_polygon",
+        },
+        TestCase {
+            network_name: "polygon",
+            input_asset_name: "usdc",
+            output_asset_name: "sol",
+            exchange_expected: "quickswap_v2",
+        },
+    ];
 
-    let busd_bsc = busd_bsc_assets.last().unwrap();
+    for test_case in test_cases {
+        let network = config.networks.get(test_case.network_name).unwrap();
 
-    let wbnb_config = config.assets.get("wbnb").unwrap();
-    let wbnb_bsc_assets: Vec<Asset> = wbnb_config
-        .new_assets_list()
-        .unwrap()
-        .into_iter()
-        .filter(|a| a.network_id() == "bsc")
-        .collect();
+        let input_asset_config = config.assets.get(test_case.input_asset_name).unwrap();
+        let input_asset_assets: Vec<Asset> = input_asset_config
+            .new_assets_list()
+            .unwrap()
+            .into_iter()
+            .filter(|a| a.network_id() == test_case.network_name)
+            .collect();
 
-    let wbnb_bsc = wbnb_bsc_assets.last().unwrap();
+        let input_asset = input_asset_assets.last().unwrap();
 
-    let bsc_network = config.networks.get("bsc").unwrap();
+        let output_asset_config = config.assets.get(test_case.output_asset_name).unwrap();
+        let output_asset_assets: Vec<Asset> = output_asset_config
+            .new_assets_list()
+            .unwrap()
+            .into_iter()
+            .filter(|a| a.network_id() == test_case.network_name)
+            .collect();
 
-    let amount_in = U256::exp10(18);
+        let output_asset = output_asset_assets.last().unwrap();
 
-    let exchange = bsc_network
-        .get_exchange_by_liquidity(wbnb_bsc, busd_bsc, amount_in)
-        .await
-        .unwrap();
+        let amount_in = U256::exp10(18);
 
-    assert_eq!(exchange.name(), "biswap");
+        let exchange = network
+            .get_exchange_by_liquidity(input_asset, output_asset, amount_in)
+            .await
+            .unwrap();
+
+        assert_eq!(exchange.name(), test_case.exchange_expected);
+    }
 }
