9cea0ac6e4887815f3c1819bb7684eb6cfd6d707
Willy Romão <willyr.goncalves@gmail.com>
2023-10-11 22:24:08 +0100

    add custom errors for state with recoverability tag

--- mfm_machine/src/state/mod.rs
+++ mfm_machine/src/state/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug)]
 enum States<T: StateHandler> {
     Setup(T),
@@ -7,6 +9,50 @@ enum States<T: StateHandler> {
 trait StateHandler {
     fn handler(&self);
 }
+
+#[derive(Debug)]
+enum StateErrorRecoverability {
+    Recoverable,
+    Unrecoverable,
+}
+
+#[derive(Debug)]
+enum StateError {
+    Unknown(StateErrorRecoverability),
+    RpcConnection(StateErrorRecoverability),
+    StorageAccess(StateErrorRecoverability),
+    OnChainError(StateErrorRecoverability),
+    OffChainError(StateErrorRecoverability),
+    ParsingInput(StateErrorRecoverability),
+}
+
+impl StateError {
+    pub fn is_recoverable(&self) -> bool {
+        match self {
+            Self::Unknown(recov) => matches!(recov, StateErrorRecoverability::Recoverable),
+            Self::RpcConnection(recov) => matches!(recov, StateErrorRecoverability::Recoverable),
+            Self::StorageAccess(recov) => matches!(recov, StateErrorRecoverability::Recoverable),
+            Self::OnChainError(recov) => matches!(recov, StateErrorRecoverability::Recoverable),
+            Self::OffChainError(recov) => matches!(recov, StateErrorRecoverability::Recoverable),
+            Self::ParsingInput(recov) => matches!(recov, StateErrorRecoverability::Recoverable),
+        }
+    }
+}
+
+impl fmt::Display for StateError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Unknown(_) => write!(f, "unknown error"),
+            Self::RpcConnection(_) => write!(f, "RPC connection error"),
+            Self::StorageAccess(_) => write!(f, "storage access error"),
+            Self::OnChainError(_) => write!(f, "on-chain error"),
+            Self::OffChainError(_) => write!(f, "off-chain error"),
+            Self::ParsingInput(_) => write!(f, "parsing input error"),
+        }
+    }
+}
+
+impl std::error::Error for StateError {}
 
 struct SetupState;
 impl StateHandler for SetupState {
@@ -33,5 +79,11 @@ mod test {
             States::Setup(t) => t.handler(),
             _ => panic!("expected Setup state"),
         }
+    }
+
+    #[test]
+    fn custom_error_to_anyhow_error() {
+        let f = |error: StateError| -> anyhow::Error { error.into() };
+        f(StateError::Unknown(StateErrorRecoverability::Unrecoverable));
     }
 }
