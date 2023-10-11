ca3185f2e203ad383a6edc0e89131edca4944f08
Willy Romão <willyr.goncalves@gmail.com>
2023-10-11 10:34:04 +0100

    remove target dir

--- mfm_machine/target/debug/deps/mfm_machine-21a899e563412c2f.d
+++
@@ -1,6 +0,0 @@
-/Users/willyrgf/dev/rust/src/github.com/willyrgf/mfm/mfm_machine/target/debug/deps/mfm_machine-21a899e563412c2f.rmeta: src/lib.rs src/state/mod.rs
-
-/Users/willyrgf/dev/rust/src/github.com/willyrgf/mfm/mfm_machine/target/debug/deps/mfm_machine-21a899e563412c2f.d: src/lib.rs src/state/mod.rs
-
-src/lib.rs:
-src/state/mod.rs:
--- mfm_machine/target/debug/deps/mfm_machine-e385ec293b8d54f2.d
+++
@@ -1,6 +0,0 @@
-/Users/willyrgf/dev/rust/src/github.com/willyrgf/mfm/mfm_machine/target/debug/deps/mfm_machine-e385ec293b8d54f2: src/lib.rs src/state/mod.rs
-
-/Users/willyrgf/dev/rust/src/github.com/willyrgf/mfm/mfm_machine/target/debug/deps/mfm_machine-e385ec293b8d54f2.d: src/lib.rs src/state/mod.rs
-
-src/lib.rs:
-src/state/mod.rs:
