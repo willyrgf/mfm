91621ee4b9d3780093493572b6d447f1421342be
Willy Romão <willyr.goncalves@gmail.com>
2023-10-16 21:39:36 +0100

    breakdown state_machine execution and add transition

--- mfm_machine/src/state/state_machine.rs
+++ mfm_machine/src/state/state_machine.rs
@@ -2,11 +2,13 @@ use std::usize;
 
 use anyhow::{anyhow, Error};
 
-use super::{context::Context, State, StateError, StateHandler, StateWrapper};
+use super::{context::Context, State, StateError};
 
 struct StateMachine {
     states: Vec<State>,
 }
+
+type StateResult = Result<(), Error>;
 
 impl StateMachine {
     pub fn new(initial_states: Vec<State>) -> Self {
@@ -19,25 +21,27 @@ impl StateMachine {
         self.states.len() > state_index
     }
 
-    pub fn execute(&self, context: &mut impl Context, state_index: usize) -> Result<(), Error> {
+    // TODO: add logging, instrumentation
+    fn transition(
+        &self,
+        _context: &mut impl Context,
+        state_index: usize,
+        last_state_result: Option<StateResult>,
+    ) -> Result<usize, Error> {
         if !self.has_state(0) {
             return Err(anyhow!("no states defined to execute"));
         }
 
-        if !self.has_state(state_index) {
-            return Ok(());
+        // if thats true, means that no state was executed before and this is the first one
+        if last_state_result.is_none() {
+            return Ok(state_index);
         }
 
-        let current_state = &self.states[state_index];
-
-        let result = match current_state {
-            State::Setup(h) => h.handler(context),
-            State::Report(h) => h.handler(context),
-        };
+        let state_result = last_state_result.unwrap();
 
         // TODO: it may be the transition
-        match result {
-            Ok(()) => self.execute(context, state_index + 1),
+        match state_result {
+            Ok(()) => Ok(state_index + 1),
             Err(e) => {
                 match e.downcast::<StateError>() {
                     Ok(se) if se.is_recoverable() => {
@@ -52,13 +56,39 @@ impl StateMachine {
             }
         }
     }
+
+    fn execute_rec(
+        &self,
+        context: &mut impl Context,
+        state_index: usize,
+        last_state_result: Option<StateResult>,
+    ) -> Result<(), Error> {
+        let next_state_index = self.transition(context, state_index, last_state_result)?;
+
+        if !self.has_state(next_state_index) {
+            return Ok(());
+        }
+
+        let current_state = &self.states[next_state_index];
+
+        let result = match current_state {
+            State::Setup(h) => h.handler(context),
+            State::Report(h) => h.handler(context),
+        };
+
+        self.execute_rec(context, next_state_index, Option::Some(result))
+    }
+
+    pub fn execute(&self, context: &mut impl Context) -> Result<(), Error> {
+        self.execute_rec(context, 0, Option::None)
+    }
 }
 
 mod test {
     #[test]
     fn test_state_machine_execute() {
         use super::*;
-        use crate::state::{context::RawContext, states};
+        use crate::state::{context::RawContext, states, StateWrapper};
 
         let initial_states = vec![
             State::Setup(StateWrapper::new(states::Setup::new())),
@@ -68,11 +98,11 @@ mod test {
         let state_machine = StateMachine::new(initial_states.clone());
 
         let mut context = RawContext::new();
-        let result = state_machine.execute(&mut context, 0);
+        let result = state_machine.execute(&mut context);
         let last_ctx_message: String = context.read().unwrap();
 
         assert_eq!(state_machine.states, initial_states);
         assert!(result.is_ok());
-        assert_eq!(last_ctx_message, "some new data".to_string());
+        assert_eq!(last_ctx_message, "some new data reported".to_string());
     }
 }
