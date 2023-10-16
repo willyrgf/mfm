b6ec10c87e2fad12ff81bb2c3e60c0b93e70181d
Willy Romão <willyr.goncalves@gmail.com>
2023-10-16 22:12:48 +0100

    add mfm_machine_macros with proc macros for state config trait requirements

---
+++ mfm_machine/mfm_machine_macros/src/lib.rs
@@ -0,0 +1,33 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(StateReqs)]
+pub fn state_reqs_derive(input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+
+    let ident = &input.ident;
+    let expanded = quote! {
+        impl StateConfig for #ident {
+            fn label(&self) -> &Label {
+                &self.label
+            }
+
+            fn tags(&self) -> &[Tag] {
+                &self.tags
+            }
+
+            fn depends_on(&self) -> &[Tag] {
+                &self.depends_on
+            }
+
+            fn depends_on_strategy(&self) -> &DependencyStrategy {
+                &self.depends_on_strategy
+            }
+        }
+    };
+
+    TokenStream::from(expanded)
+}
