be3fd520cf3888c48db2b0e01bb2840a3274baea
Willy Romão <willyr.goncalves@gmail.com>
2023-11-08 21:49:09 +0000

    move examples to tests

--- mfm_machine/src/state/context.rs
+++ mfm_machine/src/state/context.rs
@@ -1,53 +1,58 @@
 use anyhow::{anyhow, Error};
-use serde_json::{json, Value};
+use serde_json::Value;
 
 pub trait Context {
     fn read_input(&self) -> Result<Value, Error>;
-    fn write_output(&mut self, data: &Value) -> Result<(), Error>;
+    fn write_output(&mut self, value: &Value) -> Result<(), Error>;
 }
 
-// Your custom context that implements the library's Context trait.
-pub struct MyContext {
-    data: Value,
-}
+mod test {
+    use serde_derive::{Deserialize, Serialize};
 
-impl MyContext {
-    pub fn new() -> Self {
-        Self { data: json!({}) }
-    }
-}
+    use super::*;
 
-impl Context for MyContext {
-    fn read_input(&self) -> Result<Value, Error> {
-        Ok(self.data.clone())
+    #[derive(Serialize, Deserialize)]
+    struct ContextA {
+        a: String,
+        b: u64,
     }
 
-    fn write_output(&mut self, data: &Value) -> Result<(), Error> {
-        self.data = data.clone();
-        Ok(())
+    impl ContextA {
+        fn _new(a: String, b: u64) -> Self {
+            Self { a, b }
+        }
+    }
+
+    impl Context for ContextA {
+        fn read_input(&self) -> Result<Value, Error> {
+            serde_json::to_value(self).map_err(|e| anyhow!(e))
+        }
+
+        fn write_output(&mut self, value: &Value) -> Result<(), Error> {
+            let ctx: ContextA = serde_json::from_value(value.clone()).map_err(|e| anyhow!(e))?;
+            self.a = ctx.a;
+            self.b = ctx.b;
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_read_write() {
+        let context_a: &mut dyn Context = &mut ContextA::_new(String::from("hello"), 7);
+        let context_b: &dyn Context = &ContextA::_new(String::from("hellow"), 9);
+
+        assert_ne!(
+            context_a.read_input().unwrap(),
+            context_b.read_input().unwrap()
+        );
+
+        context_a
+            .write_output(&context_b.read_input().unwrap())
+            .unwrap();
+
+        assert_eq!(
+            context_a.read_input().unwrap(),
+            context_b.read_input().unwrap()
+        );
     }
 }
-
-// #[derive(Debug)]
-// pub struct RawContext {
-//     data: String,
-// }
-//
-// impl RawContext {
-//     pub fn new() -> Self {
-//         Self {
-//             data: "{}".to_string(),
-//         }
-//     }
-// }
-//
-// impl Context for RawContext {
-//     fn read<T: for<'de> Deserialize<'de>>(&self) -> Result<T, Error> {
-//         serde_json::from_str(&self.data).map_err(|e| anyhow!("error on deserialize: {}", e))
-//     }
-//
-//     fn write<T: Serialize>(&mut self, data: &T) -> Result<(), Error> {
-//         self.data = serde_json::to_string(data)?;
-//         Ok(())
-//     }
-// }
