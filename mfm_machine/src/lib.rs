834c34e01d00c63fda564adf98cce45ee5fa542c
Willy Romão <willyr.goncalves@gmail.com>
2023-11-25 22:31:00 +0000

    feat: add tracker and improve context to be trackable

--- mfm_machine/src/lib.rs
+++ mfm_machine/src/lib.rs
@@ -1,6 +1,6 @@
 pub mod state;
 pub mod state_machine;
-pub use mfm_machine_macros::StateConfigReqs;
+pub use mfm_machine_macros::StateMetadataReqs;
 
 //FIXME: reorganize library to be more ergonomic to use
 // see example in tests/public_api_test.rs how bad its.
--- mfm_machine/src/state_machine/tracker.rs
+++ mfm_machine/src/state_machine/tracker.rs
@@ -1,13 +1,12 @@
 use std::collections::HashMap;
 
 use anyhow::{anyhow, Error};
-use serde_json::Value;
 
-use crate::state::{Label, Tag};
+use crate::state::{context::ContextWrapper, Label, Tag};
 
 pub trait Tracker {
-    fn track(&mut self, index: Index, value: Value) -> Result<bool, Error>;
-    fn recover(&self, index: Index) -> Result<Value, Error>;
+    fn track(&mut self, index: Index, context: ContextWrapper) -> Result<bool, Error>;
+    fn recover(&self, index: Index) -> Result<ContextWrapper, Error>;
     // TODO: may be this Label should be tag?
     // it may be an `search_by_tag`, and to do that we need
     // to carry the tags of an state inside the index
@@ -17,11 +16,12 @@ pub trait Tracker {
     fn search_by_tag(&self, tag: &Tag) -> Vec<Index>;
 }
 
+// TODO: should it be public? may export methods to access it
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct Index {
-    state_index: usize,
-    state_label: Label,
-    state_tags: Vec<Tag>,
+    pub state_index: usize,
+    pub state_label: Label,
+    pub state_tags: Vec<Tag>,
 }
 
 impl Index {
@@ -34,7 +34,7 @@ impl Index {
     }
 }
 
-pub struct HashMapTracker(HashMap<Index, Value>);
+pub struct HashMapTracker(HashMap<Index, ContextWrapper>);
 
 impl HashMapTracker {
     pub fn new() -> Self {
@@ -43,11 +43,11 @@ impl HashMapTracker {
 }
 
 impl Tracker for HashMapTracker {
-    fn track(&mut self, index: Index, value: Value) -> Result<bool, Error> {
-        Ok(self.0.insert(index, value).is_none())
+    fn track(&mut self, index: Index, context: ContextWrapper) -> Result<bool, Error> {
+        Ok(self.0.insert(index, context).is_none())
     }
 
-    fn recover(&self, index: Index) -> Result<Value, Error> {
+    fn recover(&self, index: Index) -> Result<ContextWrapper, Error> {
         Ok(self
             .0
             .get(&index)
@@ -67,20 +67,49 @@ impl Tracker for HashMapTracker {
 
 #[cfg(test)]
 mod test {
-    use serde_json::json;
+    use anyhow::{anyhow, Error};
+    use serde_derive::{Deserialize, Serialize};
+    use serde_json::Value;
 
-    use crate::state::{Label, Tag};
+    use crate::state::{
+        context::{wrap_context, Context, ContextWrapper},
+        Label, Tag,
+    };
 
     use super::{HashMapTracker, Index, Tracker};
 
+    #[derive(Serialize, Deserialize)]
+    struct ContextA {
+        a: String,
+        b: u64,
+    }
+
+    impl ContextA {
+        fn new(a: String, b: u64) -> Self {
+            Self { a, b }
+        }
+    }
+
+    impl Context for ContextA {
+        fn read_input(&self) -> Result<Value, Error> {
+            serde_json::to_value(self).map_err(|e| anyhow!(e))
+        }
+
+        fn write_output(&mut self, value: &Value) -> Result<(), Error> {
+            let ctx: ContextA = serde_json::from_value(value.clone()).map_err(|e| anyhow!(e))?;
+            self.a = ctx.a;
+            self.b = ctx.b;
+            Ok(())
+        }
+    }
     #[test]
     fn test_tracker() {
         let tracker: &mut dyn Tracker = &mut HashMapTracker::new();
 
-        let values = vec![
-            json!({"value": 1}),
-            json!({"value": 2}),
-            json!({"value": 3}),
+        let contexts: Vec<ContextWrapper> = vec![
+            wrap_context(ContextA::new("value".to_string(), 1)),
+            wrap_context(ContextA::new("value".to_string(), 2)),
+            wrap_context(ContextA::new("value".to_string(), 3)),
         ];
         let indexes = vec![
             Index::new(
@@ -102,13 +131,17 @@ mod test {
 
         for i in 0..indexes.len() {
             tracker
-                .track(indexes[i].clone(), values[i].clone())
+                .track(indexes[i].clone(), contexts[i].clone())
                 .unwrap();
         }
 
         for i in 0..indexes.len() {
-            let value_recovered = tracker.recover(indexes[i].clone()).unwrap();
-            assert_eq!(values[i], value_recovered);
+            let context_recovered = tracker.recover(indexes[i].clone()).unwrap();
+
+            let value_recovered = context_recovered.lock().unwrap().read_input().unwrap();
+            let value_expected = contexts[i].lock().unwrap().read_input().unwrap();
+
+            assert_eq!(value_expected, value_recovered);
         }
     }
 
@@ -116,10 +149,10 @@ mod test {
     fn test_search_by_tag() {
         let tracker: &mut dyn Tracker = &mut HashMapTracker::new();
 
-        let values = vec![
-            json!({"value": 1}),
-            json!({"value": 2}),
-            json!({"value": 3}),
+        let contexts: Vec<ContextWrapper> = vec![
+            wrap_context(ContextA::new("value".to_string(), 1)),
+            wrap_context(ContextA::new("value".to_string(), 2)),
+            wrap_context(ContextA::new("value".to_string(), 3)),
         ];
         let indexes = vec![
             Index::new(
@@ -141,7 +174,7 @@ mod test {
 
         for i in 0..indexes.len() {
             tracker
-                .track(indexes[i].clone(), values[i].clone())
+                .track(indexes[i].clone(), contexts[i].clone())
                 .unwrap();
         }
 
