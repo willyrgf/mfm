d5eb3b5cf6427beaca3d111137d7eb301ce99ee3
Willy Romão <willyr.goncalves@gmail.com>
2024-02-07 10:43:29 +0000

    refactor: cleanup passphrase and hidden impl to use tari_utils (#67)

--- mfm_core/src/config/authentication/wallet.rs
+++ mfm_core/src/config/authentication/wallet.rs
@@ -1,11 +1,26 @@
-use serde_derive::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
+use tari_utilities::SafePassword;
 
-use crate::password::{deserialize_safe_password, SafePassword};
-
-#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Wallet {
     #[serde(deserialize_with = "deserialize_safe_password")]
     pub private_key: SafePassword,
     pub not_encrypted: bool,
     pub env_password: Option<String>,
+}
+
+impl PartialEq for Wallet {
+    fn eq(&self, other: &Self) -> bool {
+        self.not_encrypted == other.not_encrypted && self.env_password == other.env_password
+    }
+}
+
+impl Eq for Wallet {}
+
+fn deserialize_safe_password<'de, D>(deserializer: D) -> Result<SafePassword, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let password: String = Deserialize::deserialize(deserializer)?;
+    Ok(SafePassword::from(password))
 }
--- mfm_core/src/lib.rs
+++ mfm_core/src/lib.rs
@@ -1,6 +1,3 @@
 pub mod config;
 pub mod contexts;
-pub mod hidden;
-pub mod password;
 pub mod states;
-pub mod tasks;
