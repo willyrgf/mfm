4c27d27e0edf6dc570f6ba1a2b2526c6e309210b
Willy Romão <willyr.goncalves@gmail.com>
2026-02-10 16:25:41 +0700

    redesign: remove legacy machine

--- crates/machine-legacy/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod state;
-pub mod state_machine;
-// Allow proc-macro expansions to refer to this crate by name (even within the crate itself).
-extern crate mfm_machine_derive_legacy;
-extern crate self as mfm_machine_legacy;
-
-//FIXME: reorganize library to be more ergonomic to use
-// see example in tests/public_api_test.rs how bad its.
