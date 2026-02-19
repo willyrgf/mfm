e117f5e963ab34761c6869c0868899d0ef9a2ae0
Willy Romão <willyr.goncalves@gmail.com>
2026-02-19 11:11:31 +0700

    ci: run parity portfolio snapshot test in full lane

--- nixfied/project/ci/scripts/steps/parity-evm-reth.nix
+++ nixfied/project/ci/scripts/steps/parity-evm-reth.nix
@@ -15,5 +15,5 @@ pkgs.writeText "mfm-ci-steps-parity-evm-reth.sh" ''
   run_hook SVC_MINIO_BUCKET_ENSURE "$MINIO_BUCKET"
 
   LOGFILE=$(artifact_path "parity-evm-reth.log")
-  log_capture "$LOGFILE" -- cargo-nightly nextest run --cargo-profile ci --jobs 1 -p mfm-integration-tests --features parity-tests --test parity_rest_api_evm_reth_pipeline --test parity_portfolio_tracker_reth_mock_erc20
+  log_capture "$LOGFILE" -- cargo-nightly nextest run --cargo-profile ci --jobs 1 -p mfm-integration-tests --features parity-tests --test parity_rest_api_evm_reth_pipeline --test parity_portfolio_tracker_reth_mock_erc20 --test parity_portfolio_tracker_reth_snapshot
 ''
