8416469210a0ee33abdf70c66091a865e43e4518
Willy Romão <willyr.goncalves@gmail.com>
2026-07-01 13:16:35 -0300

    tests: remove removed api trybuild tombstones

--- crates/kernel/program/tests/ui/fail/context_key_state_input.rs
+++ crates/kernel/program/tests/ui/fail/context_key_state_input.rs
@@ -1,7 +1,7 @@
 struct ContextKey(String);
 
 fn main() {
-    let key = ContextKey("old.context.key".to_owned());
+    let key = ContextKey("invalid.context.key".to_owned());
     let _ = <ContextKey as mfm_program::IntoStateInput<'static, 'static, ContextKey>>::into_binding(
         key,
     );
